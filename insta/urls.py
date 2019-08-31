979974274bc46de6629e7c90d10642f5b5684909
nimosaid <saidnimo123@gmail.com>
2019-08-31 23:40:16 +0300

    add

--- insta/urls.py
+++ insta/urls.py
@@ -1,21 +1,15 @@
-"""insta URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
-from django.contrib import admin
+from . import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$',views.home,name='home'),
+    url(r'^newprofile/',views.profile,name ='profile'),
+    url(r'^showprofile/(?P<id>\d+)',views.display_profile,name = 'showprofile'),
+    url(r'^image/$', views.add_image, name='upload_image'),
+    url(r'^ search/',views.search, name='search'),
+    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
+    url(r'^follow/(?P<user_id>\d+)', views.follow, name='follow'),
+
 ]
