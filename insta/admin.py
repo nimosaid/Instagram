f0957b770098c77794ce73bb349b5095176b55a7
nimosaid <saidnimo123@gmail.com>
2019-09-01 13:25:27 +0300

    working

--- insta/admin.py
+++ insta/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import Image,Profile,Comment
+# from django.contrib import admin
+# from .models import Image,Profile,Comment
 
-# Register your models here.
-admin.site.register(Image)
-admin.site.register(Profile)
-admin.site.register(Comment)
+# # Register your models here.
+# admin.site.register(Image)
+# admin.site.register(Profile)
+# admin.site.register(Comment)
