ed476bc1bdabdc864223633e7c988a94f6cb708c
nimosaid <saidnimo123@gmail.com>
2019-09-01 13:31:37 +0300

    working

--- insta/forms.py
+++ insta/forms.py
@@ -1,21 +1,21 @@
-from django import forms
-from .models import Profile,Image,Comment
+# from django import forms
+# from .models import Profile,Image,Comment
 
 
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        exclude=['owner']
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         exclude=['owner']
 
 
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = Image
-        exclude =['likes','profile']
+# class ImageForm(forms.ModelForm):
+#     class Meta:
+#         model = Image
+#         exclude =['likes','profile']
 
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        exclude = ['image', 'comment_owner']
+# class CommentForm(forms.ModelForm):
+#     class Meta:
+#         model = Comment
+#         exclude = ['image', 'comment_owner']
 
